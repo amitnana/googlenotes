f4de43098b169a0200b2bfd58c61191ce4f2c9a7
amitnana <amittambe2233@gmail.com>
2018-03-12 12:54:09 +0530

    deployed file

--- mynote/api/urls.py
+++ mynote/api/urls.py
@@ -7,10 +7,10 @@ from . import views
 
 urlpatterns =[
 
-  url(r'^api/notes',views.ListNote.as_view()),
+  url(r'^',views.ListNote.as_view()),
   url(r'^api/note/(?P<pk>\d+)/edit/',views.UpdateNote.as_view()),
   url(r'^api/note/(?P<pk>\d+)/delete/',views.DeleteNote.as_view()),
   url(r'api/create',views.CreateNote.as_view())
-  
+
 
 ]
